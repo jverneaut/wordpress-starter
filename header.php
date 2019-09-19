282b3134c22bc1f0899ddd0497677173fe2ba052
Julien Verneaut <julienverneaut@MacBook-Pro-de-Julien.local>
2019-09-19 11:32:39 +0200

    Use wp title

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>77</title>
+  <title><?= wp_title(''); ?></title>
   <?php wp_head(); ?>
 </head>
 <body>
