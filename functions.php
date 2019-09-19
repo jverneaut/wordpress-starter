d973643c2113c08b8c2c3e0a4f82c08f487949c5
Julien Verneaut <julienverneaut@MacBook-Pro-de-Julien.local>
2019-09-19 12:03:20 +0200

    Format PHP and setup title

--- functions.php
+++ functions.php
@@ -1,11 +1,20 @@
 <?php
 
-define('THEME_URL', get_bloginfo('template_url') );
-define('IMG_URL', THEME_URL.'/img');
+define('THEME_URL', get_bloginfo('template_url'));
+define('IMG_URL', THEME_URL . '/img');
 
-function load_scripts_styles() {
+function load_scripts_styles()
+{
   wp_enqueue_script('theme-js', THEME_URL . '/dist/bundle.js', null, null, true);
   wp_enqueue_style('theme-css', THEME_URL . '/dist/style.css');
 }
 
 add_action('wp_enqueue_scripts', 'load_scripts_styles');
+
+
+function setup_theme()
+{
+  add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'setup_theme');
--- header.php
+++ header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title><?= wp_title(''); ?></title>
   <?php wp_head(); ?>
 </head>
+
 <body>
